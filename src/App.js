931aab4bf221a2aeecc51ec9ca248c943bad3008
Stanislav Kruhliak <stanislav.kruhliak@gmail.com>
2021-07-05 18:06:33 +0200

    fix markup/styles

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default class App extends Component {
     const { good, neutral, bad } = this.state;
     const btnNames = Object.keys(this.state);
     return (
-      <div>
+      <div className="container">
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={btnNames}
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,12 +1,14 @@
+import { Thumb } from './Statistics.styles';
+
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
   return (
-    <div>
+    <Thumb>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <p>Total: {total()}</p>
       <p>Positive feedback: {good && positivePercentage()}%</p>
-    </div>
+    </Thumb>
   );
 }
 
